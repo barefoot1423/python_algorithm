6b22bcc0d8923e8f6ea14f123785cf6cfbe43e41
Hyunwoo Cho <barefoot1423@gmail.com>
2021-11-15 22:51:24 -0500

    [001] optimal solution

---
+++ leecode/p001/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_map = dict()
+        for i, x in enumerate(nums):
+            if target - x in index_map:
+                return [index_map[target-x], i]
+
+            index_map[x] = i
+
+        return None
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum([1,2,3,1,4,7,2,3], 11))
